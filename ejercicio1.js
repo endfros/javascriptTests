ffb35ed4e22a6d6933a508cf5b7146f7b843a4a5
Rodrigo Montoya <montoyar32@gmail.com>
2022-08-10 21:37:37 -0500

    First version of the first exercise

---
+++ ejercicio1.js
@@ -0,0 +1,10 @@
+let var1 = 2;
+let var2 = 3;
+
+if (var1 > var2){
+    console.log("El número 1 es mayor")
+} else if (var1 < var2){
+    console.log("El número 2 es mayor")
+} else {
+    console.log("Los números son iguales")
+}
---
+++ operadores.js
@@ -0,0 +1,10 @@
+let numero1 = 10;
+let numero2 = -10;
+
+// console.log(numero1 * numero2);
+console.log(++numero1);
+
+let logueado = null;
+let tieneTDC = true;
+
+console.log(logueado && tieneTDC);
