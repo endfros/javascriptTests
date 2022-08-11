9a9e3b65004ba3068d13f92d36fe4f8441bd501f
Rodrigo Montoya <montoyar32@gmail.com>
2022-08-10 22:04:24 -0500

    Added Clase2 Folder

---
+++ Clase2/functions.js
@@ -0,0 +1,13 @@
+// Declarativa
+function miFuncion(texto1, texto2){
+    // cada elemento adentro del parentesis son parametros. a cada
+    // elemento se le tiene que poner un nombre que de a entender
+    // que es cada uno
+    console.log(texto1+texto2);
+}
+
+// expresion
+
+let miVariableEsFunction = function(){
+
+}
