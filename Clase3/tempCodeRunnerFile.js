6793121ddab5b19472168bb11db4a95fb27dc0ad
Rodrigo Montoya <montoyar32@gmail.com>
2022-08-12 20:13:46 -0500

    Updated ejercicioEnClase

--- Clase3/tempCodeRunnerFile.js
+++ Clase3/tempCodeRunnerFile.js
@@ -1,7 +1,11 @@
-let frutas = ['manzana', 'platano', 'fresa', 'melon'];
-let n = 0;
-
-while(n <= (frutas.length-1)){
-    console.log(frutas[n]);
-    n++;
+function imprimirFrutas(){
+    let frutas = ['manzana', 'platano', 'fresa', 'melon'];
+    let n = 0;
+    
+    while(n <= (frutas.length-1)){
+        console.log(frutas[n]);
+        n++;
+    }
 }
+
+imprimirFrutas();
